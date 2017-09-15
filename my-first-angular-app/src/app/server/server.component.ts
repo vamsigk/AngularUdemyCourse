4beac544b293c37f91d2038c296d926c37f7bafb
vamsi <vamsi.gpv@gmail.com>
2017-09-15 17:07:04 -0400

    added bootstrap and tested basic binding

--- my-first-angular-app/src/app/server/server.component.ts
+++ my-first-angular-app/src/app/server/server.component.ts
@@ -9,6 +9,35 @@ export class ServerComponent  {
 	
 	serverId: number = 10;
 	serverStatus: string = 'online';
-  
+	serverName = '';
+	userName = '';
+	isUserNameEmpty: boolean = true;
 
+	onAddServerClick() {
+		if(this.serverStatus == 'online') {
+			this.serverStatus = 'Offline';
+		}else {
+			this.serverStatus = 'online';
+		}
+		
+	}
+
+	onTextInput(event: Event) {
+		console.log("Event value is : " + (<HTMLInputElement>event.target).value)
+		this.serverName = (<HTMLInputElement>event.target).value;
+	}
+
+	onTwoWayBindingClick(event: Event) {
+		console.log("Username value is : " + (<HTMLInputElement>event.target).value)
+		this.userName = (<HTMLInputElement>event.target).value;
+		this.checkUserNameEmpty();
+	}
+
+	checkUserNameEmpty() {
+		if(this.userName == '') {
+			this.isUserNameEmpty = true;
+		}else {
+			this.isUserNameEmpty = false;
+		}
+	}
 }
